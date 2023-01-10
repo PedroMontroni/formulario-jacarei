c0019adb1cc0b786dd93b643816d835eb390bb19
Pedro Montroni <pedromontroni@gmail.com>
2023-01-10 20:05:52 -0300

    arquivos iniciais

---
+++ enviar-email.php
@@ -0,0 +1,16 @@
+<?php
+  //Variáveis
+  $ nome = $ _POST [ 'nome' ];
+  $ email = $ _POST [ 'email' ];
+  $ mensagem = $ _POST [ 'mensagem' ];
+  $ data_envio = data( 'd/m/Y' );
+  $ hora_envio = data( 'H:i:s' );
+
+
+?>
+Rodapé
+© 2023 GitHub, Inc.
+Navegação no rodapé
+Termos
+Privacidade
+Segura
